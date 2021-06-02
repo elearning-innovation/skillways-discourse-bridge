472dcee02c3b888ac4d41115fd7ee6bd2fd2935e
Daniel Del Rio <ddelrio1986@gmail.com>
2021-06-02 13:36:21 -0400

    Add new discussion preparation approach

--- app/controllers/skillways_discourse_bridge/actions_controller.rb
+++ app/controllers/skillways_discourse_bridge/actions_controller.rb
@@ -5,6 +5,20 @@ module SkillwaysDiscourseBridge
     before_action :ensure_logged_in
 
     def index
+      templateCategoryId = params[:templateCategoryId]
+      uniqueCategoryIdentifier = params[:uniqueCategoryIdentifier]
+
+      categoryExists = Category.exists?(:name => uniqueCategoryIdentifier)
+      unless categoryExists
+        category = Category.new(:name => uniqueCategoryIdentifier)
+        # copy topics
+      else
+       category = Category.where(:name => uniqueCategoryIdentifier)
+      end
+
+      # check the number of topics in the category
+      # if the category has multiple topics then redirect to the category
+      # if the category has one topic then redirect to the topic
       render_json_dump({ actions: [] })
     end
 
