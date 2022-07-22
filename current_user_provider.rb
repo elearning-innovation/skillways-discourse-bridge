3a5d61c2f4642a6b7058ae5666f15f3a81f5b0b9
Daniel Del Rio <ddelrio1986@gmail.com>
2022-07-22 09:03:04 -0400

    Change cookie settings

--- current_user_provider.rb
+++ current_user_provider.rb
@@ -2,18 +2,7 @@ class SkillwaysCurrentUserProvider < Auth::DefaultCurrentUserProvider
   def cookie_hash(unhashed_auth_token)
     hash = {
       value: unhashed_auth_token,
-      httponly: true,
-      secure: SiteSetting.force_https,
-      domain: '*.google.com'
     }
-
-    if SiteSetting.persistent_sessions
-      hash[:expires] = SiteSetting.maximum_session_age.hours.from_now
-    end
-
-    if SiteSetting.same_site_cookies != "Disabled"
-      hash[:same_site] = SiteSetting.same_site_cookies
-    end
 
     hash
   end
