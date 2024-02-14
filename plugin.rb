0540a7f87bfbab2222d605abf6fa4e147c6c0861
Daniel Del Rio <ddelrio1986@gmail.com>
2024-02-14 12:58:34 -0500

    Remove old code

--- plugin.rb
+++ plugin.rb
@@ -5,8 +5,6 @@
 # version: 1.3.1
 # authors: Skillways
 
-#gem 'jwt', '2.2.3' # local dev
-#gem 'jwt', '2.4.1' # old production/staging value
 gem 'jwt', '2.7.1'
 
 enabled_site_setting :skillways_discourse_bridge_enabled
